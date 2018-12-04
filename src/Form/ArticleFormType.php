2050ea9584380694972baa58c280f25b59db5153
Romain Helou <helouromain@gmail.com>
2018-12-04 18:15:09 +0100

    6 - DateTimeType & Data Transforming

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -24,6 +24,9 @@ class ArticleFormType extends AbstractType
                 'help' => 'Choose something catchy!',
             ])
             ->add('content')
+            ->add('publishedAt', null, [
+                'widget' => 'single_text',
+            ])
         ;
     }
 
