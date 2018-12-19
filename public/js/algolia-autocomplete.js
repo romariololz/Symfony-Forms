9160f25c6a522fdd788e6941ab183364bf6170aa
Romain Helou <helouromain@gmail.com>
2018-12-19 14:01:32 +0100

    29 - Hooking up the AJAX Autocomplete

--- public/js/algolia-autocomplete.js
+++ public/js/algolia-autocomplete.js
@@ -1,12 +1,21 @@
 $(document).ready(function () {
-    $('.js-user-autocomplete').autocomplete({hint: false}, [
-        {
-            source: function (query, cb) {
-                cb([
-                    {value: 'foo'},
-                    {value: 'bar'}
-                ])
+    $('.js-user-autocomplete').each(function () {
+        var autocompleteUrl = $(this).data('autocomplete-url');
+
+        $(this).autocomplete({hint: false}, [
+            {
+                source: function (query, cb) {
+                    cb([
+                        $.ajax({
+                            url: autocompleteUrl+'?query='+query
+                        }).then(function (data) {
+                            cb(data.users)
+                        })
+                    ])
+                },
+                displayKey: 'email',
+                debounce: 500
             }
-        }
-    ])
+        ])
+    })
 });
--- src/Form/UserSelectTextType.php
+++ src/Form/UserSelectTextType.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Routing\RouterInterface;
 
 class UserSelectTextType extends AbstractType
 {
@@ -23,9 +24,15 @@ class UserSelectTextType extends AbstractType
      */
     private $userRepository;
 
-    public function __construct(UserRepository $userRepository)
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    public function __construct(UserRepository $userRepository, RouterInterface $router)
     {
         $this->userRepository = $userRepository;
+        $this->router = $router;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -49,7 +56,8 @@ class UserSelectTextType extends AbstractType
                 return $userRepository->findOneBy(['email' => $email]);
             },
             'attr' => [
-                'class' => 'js-user-autocomplete'
+                'class' => 'js-user-autocomplete',
+                'data-autocomplete-url' => $this->router->generate('admin_utility_users')
             ]
         ]);
     }
