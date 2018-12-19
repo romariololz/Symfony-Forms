26d832728d9018daee483e54d834e2cdbf768c45
Romain Helou <helouromain@gmail.com>
2018-12-19 14:30:33 +0100

    31 - Form Type Extension

---
+++ src/Form/TypeExtension/TextareaSizeExtension.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romariololz
+ * Date: 19/12/18
+ * Time: 14:19
+ */
+
+namespace App\Form\TypeExtension;
+
+
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormTypeExtensionInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TextareaSizeExtension implements FormTypeExtensionInterface
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['attr']['rows'] = $options['rows'];
+    }
+
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'rows' => 10
+        ]);
+    }
+
+    public function getExtendedType()
+    {
+        return TextareaType::class;
+    }
+}
