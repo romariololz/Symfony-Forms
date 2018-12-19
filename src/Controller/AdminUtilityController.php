50f3be057cbcef16ed4f758cb938cd3c82c43b03
Romain Helou <helouromain@gmail.com>
2018-12-19 13:43:14 +0100

    28 - Autocomplete Endpoint & Serialization Group

---
+++ src/Controller/AdminUtilityController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romariololz
+ * Date: 19/12/18
+ * Time: 13:35
+ */
+
+namespace App\Controller;
+
+
+use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminUtilityController extends AbstractController
+{
+    /**
+     * @Route("/admin/utility/users", methods="GET")
+     * @IsGranted("ROLE_ADMIN_ARTICLE")
+     */
+    public function getUsersApi(UserRepository $userRepository)
+    {
+        $users = $userRepository->findAllEmailAlphabetical();
+
+        return $this->json([
+            'users' => $users
+        ], 200, [], ['groups' => ['main']]);
+    }
+}
