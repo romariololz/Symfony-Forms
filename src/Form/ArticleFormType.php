3fac17024f6196fb4464ee1b9fdb225263a8f969
Romain Helou <helouromain@gmail.com>
2018-12-21 14:24:16 +0100

    35 - Setup: For Dependent Select Fields

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -14,6 +14,7 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -44,6 +45,22 @@ class ArticleFormType extends AbstractType
             ])
             ->add('author', UserSelectTextType::class, [
                 'disabled' => $isEdit
+            ])
+            ->add('location', ChoiceType::class, [
+                'placeholder' => 'Choose a location',
+                'choices' => [
+                    'The Solar System' => 'solar_system',
+                    'Near a start' => 'star',
+                    'Interstellar Space' => 'interstellar_space'
+                ],
+                'required' => false,
+            ])
+            ->add('specificLocationName', ChoiceType::class, [
+                'placeholder' => 'Where exactly?',
+                'choices' => [
+                    'TODO' => 'TODO'
+                ],
+                'required' => false,
             ])
         ;
 
