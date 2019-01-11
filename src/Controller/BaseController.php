012f955272f8ed81075ad4e9dd59e0b0f7561563
Romain Helou <helouromain@gmail.com>
2019-01-11 14:06:53 +0100

    41 - A bit of Security Cleanup

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * Class BaseController
+ * @package App\Controller
+ * @method User getUser()
+ */
 abstract class BaseController extends AbstractController
 {
-    protected function getUser(): User
-    {
-        return parent::getUser();
-    }
 }
